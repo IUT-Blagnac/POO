4e16124b99b725413ca53b377de06801a17d4a15
Péninou <andre.peninou@univ-tlse2.fr>
2014-01-27 16:04:04 -0800

    TD1 André. J'ai enlevé htmlGenerated : a voir, je corrigerai. Reste à enlever global.asc

---
+++ POO/src/HelloMonde.java
@@ -0,0 +1,7 @@
+
+public class HelloMonde {
+	public static void main (String argv[])
+	{ //2
+		System.out.println ("Hello World !"); //1
+	}
+}
---
+++ POO/src/old/td1Ex2.java
@@ -0,0 +1,13 @@
+public static void main(String argv[]) {
+	int i, j, k ;
+	double d, e, f
+	i = 1; 
+	j = i + 10 ; 
+	k = j ; 
+	e = 10.5; 
+	// <1>
+	if (e < i) { 
+		f = j + k ;
+		k = (int) (f - e) ;
+	}	
+}
