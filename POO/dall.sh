2cc05aedb3bedfebff1fa700463b99c346e46c14
Jean-Michel Bruel <jbruel@gmail.com>
2014-01-30 16:42:04 +0100

    plan du cours

--- POO/dall.sh
+++ POO/dall.sh
@@ -1,5 +1,4 @@
-
-rm  htmlGenerated/$1_Sujet.html   
+rm  htmlGenerated/$1_Sujet.html
 rm  htmlGenerated/$1_Support.html
 rm  htmlGenerated/$1_Profs.html
 rm  htmlGenerated/$1_SupportSlides.html
--- POO/gprintable.sh
+++ POO/gprintable.sh
@@ -1,4 +1,3 @@
-
 asciidoc   -a toc   -a data-uri   -a icons   -a eleve  \
 	-a stylesheet=stylesheets/golo-jmb.css  -a impression   \
 	-o htmlGenerated/$1_SujetPrintable.html   $1.asc
