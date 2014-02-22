6e466918afca2a0c8c530be0c694575d0f52ce8c
Andre Peninou <andre.peninou@irit.fr>
2014-02-22 16:55:28 +0100

    maj TD4

--- POO/dall.sh
+++ POO/dall.sh
@@ -3,6 +3,5 @@ rm  htmlGenerated/$1_Support.html
 rm  htmlGenerated/$1_Profs.html
 rm  htmlGenerated/$1_SupportSlides.html
 
-# rm htmlGenerated/$1_SujetPrintable.html 
-
+rm htmlGenerated/$1_SujetPrintable.html
 
