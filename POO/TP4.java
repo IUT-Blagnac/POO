60dd7bfa1760ec5893c1a94681a7fffb04924862
Andre Peninou <andre.peninou@univ-tlse2.fr>
2015-03-04 09:25:48 +0100

    POO maj TP4

--- POO/TP4.java
+++ POO/TP4.java
@@ -7,7 +7,15 @@ public class Tp4 {
 	
 	public static void main (String argv[]) {
 		
-		// Ici.
+		// Ici :
+
+		// Déclarer une variable alChaines de type ArrayList de String.
+
+		// Créer avec alChaines une ArrayList de String avec le constructeur par défaut.
+		
+		// ...
+		
+		
 		
 		
 	}
