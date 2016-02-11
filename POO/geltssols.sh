85437de46258a869096065ba0ef44a5f44212267
Andre Peninou <andre.peninou@univ-tlse2.fr>
2016-02-11 10:57:58 +0100

    Création Elts Sol TD2

---
+++ POO/geltssols.sh
@@ -0,0 +1,8 @@
+
+asciidoc   -a toc   -a data-uri   -a icons   -a prof  \
+	-a impression  \
+	-o htmlGenerated/$1_EltsSolution.html $1.asc
+
+#		-a stylesheet=stylesheets/golo-jmb.css  -a impression  \
+
+
