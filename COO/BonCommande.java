1dea6df547e649ce68550ef59e5b6b5c2e29411b
Jean-Michel Bruel <jbruel@gmail.com>
2016-06-08 08:49:16 +0200

    bilan 2016 COO

---
+++ COO/BonCommande.java
@@ -0,0 +1,14 @@
+package m;
+
+import java.io.Serializable;
+
+public class BonCommande implements Serializable{
+	private static final long serialVUID = -4243...;
+	private long ref;
+	private String motif;
+	private String obs;
+	private double montant;
+
+...
+
+}
